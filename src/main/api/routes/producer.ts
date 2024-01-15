6e39922361d59edc1c3693eace68125a8a2db014
Hytalo <hytalo48@gmail.com>
2024-01-15 10:46:53 -0300

    add list producer by id route

--- src/main/api/routes/producer.ts
+++ src/main/api/routes/producer.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express'
-// producer create
-import { ProducerCreateController } from '@/application/controllers/producerCreate'
 import { PrismaClient } from '@prisma/client'
-import { ProducerCreateRepository } from '@/data/contracts/repositories/producerCreate'
-import { ProducerCreateService } from '@/data/services/producerCreate'
-// producer update
-import { ProducerListRepository, ProducerUpdateRepository } from '@/data/contracts/repositories'
-import { ProducerListService, ProducerUpdateService } from '@/data/services'
-import { ProducerListCrontroller, ProducerUpdateController } from '@/application/controllers'
-// producer list all
+import {
+  ProducerCreateRepository,
+  ProducerListOnceRepository,
+  ProducerListRepository,
+  ProducerUpdateRepository
+} from '@/data/contracts/repositories'
+import {
+  ProducerCreateService,
+  ProducerListOnceService,
+  ProducerListService,
+  ProducerUpdateService
+} from '@/data/services'
+import {
+  ProducerCreateController,
+  ProducerListCrontroller,
+  ProducerListOnceController,
+  ProducerUpdateController
+} from '@/application/controllers'
 
 export const producerRouter = Router()
 const prisma = new PrismaClient()
@@ -27,3 +36,8 @@ const producerListRepository = new ProducerListRepository(prisma)
 const producerListService = new ProducerListService(producerListRepository)
 const producerListController = new ProducerListCrontroller(producerListService)
 producerRouter.get('/producer', async (req, res) => producerListController.handle(req, res))
+// producer list by id
+const producerListOnceRepository = new ProducerListOnceRepository(prisma)
+const producerListOnceService = new ProducerListOnceService(producerListOnceRepository)
+const producerListOnceController = new ProducerListOnceController(producerListOnceService)
+producerRouter.get('/producer/:id', async (req, res) => producerListOnceController.handle(req, res))
